e984db0263940233fce85f1037a3209104e8a40a
Johnedel S. Mapa <johnedel.mapa@sun-asterisk.com>
2020-12-27 21:31:24 +0800

    Impelemt CRUD

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,53 @@
+class TopicsController < ApplicationController
+    def index
+      @topics = Topic.all
+    end
+  
+    def new
+      @topic = Topic.new
+    end
+  
+    def create
+      @topic = Topic.new(topics_params)
+      if @topic.save
+        redirect_to root_url
+      else
+        render 'new'
+      end
+    end
+  
+    def show
+      @topic = Topic.find(params[:id])
+    end
+  
+    def edit
+      @topic = Topic.find(params[:id])
+    end
+  
+    def update
+      @topic = Topic.find(params[:id])
+      if @topic.update(topics_params)
+        redirect_to root_url
+      else
+        render 'edit'
+      end
+    end
+  
+    def destroy
+      @topic = Topic.find(params[:id])
+      @topic.destroy
+      redirect_to root_url
+    end
+  
+    def upvote
+      @topic = Topic.find(params[:id])
+      @topic.votes.create
+      redirect_to root_url
+    end
+  
+    private
+      def topics_params
+        params.require(:topic).permit(:title, :description)
+      end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "topics#index"
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
 end
